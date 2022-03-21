2d86aae1f40b1a7e528d5922b4d3db49a650e766
maximmat <maximmatous@seznam.cz>
2022-03-21 13:25:25 +0100

    Přidejte soubory projektu.

---
+++ samostatna_prace.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int soucet(cislo) {
+    int suda = 0;
+    for (int i = 0; i <= cislo; i += 2) {
+        suda += i;
+    }
+    return suda;
+}
+
+int main() {
+    printf("Soucet sudych: %d\n", soucet(5));
+}
